381002a41966941aecfa94b21a3269118865c9f6
bhav1kmaru <bhavik.m01@gmail.com>
2023-04-29 15:02:49 +0530

    created user and post model

---
+++ model/Post.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  text: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+const PostModel=mongoose.model("Post", postSchema)
+module.exports = {PostModel};
